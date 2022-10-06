d9289922acda3e9a126816416720fae764dec6ef
MSBarbieri <matheussouzabarbieri@gmail.com>
2022-10-06 00:41:11 -0300

    change base blog url

--- components/links.tsx
+++ components/links.tsx
@@ -14,7 +14,7 @@ const Links: React.FC = () => {
         LinkedIn
       </a>
       </li >
-      < li className="bg-[#252931AA] hover:bg-[#353941CC] py-2 rounded-full my-6 drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)]" > <a className="flex items-center justify-center gap-3" href="https://www.msbarbieri.dev">
+      < li className="bg-[#252931AA] hover:bg-[#353941CC] py-2 rounded-full my-6 drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)]" > <a className="flex items-center justify-center gap-3" href="https://blog.msbarbieri.dev">
         <Image
           src="/home.svg"
           alt="Home Site"
